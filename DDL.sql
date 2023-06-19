92f94b6250038557c6b4e21460ba3cecfd5b5d0d
Aditya <asingh_me21@thapar.edu>
2023-06-20 03:13:29 +0530

    3 commit

--- DDL.sql
+++ DDL.sql
@@ -27,3 +27,12 @@ TRUNCATE TABLE users;
 
 -- DROP TABLE - puri table uda dega column + rows everything
 DROP TABLE IF EXISTS users;
+
+-- Contrains for data integrity
+-- 1- NOT NULL - koi null value allowed ni hai eg phone no
+-- 2 - UNIQUE - no dupilcate values allowed
+-- 3 - Primary key
+-- 4 - AUTO INCREMENT -  add one value eg - user id, jaisi naya user aya purani id me increment hoke nai id ban gai
+-- 5 - CHECK - on coloumn/set of columns applies a codidtion eg persons of age >30 only
+-- 6 - DEFAULT - put some default value eg - date column, just put today date by default
+-- 7 - Foreighn key
